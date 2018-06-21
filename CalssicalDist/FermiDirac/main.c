036020f83390a2bef69a415df1352e9d49f4c2ec
Rajendra Adhikari <rpadhikari@gmail.com>
2018-06-21 10:57:12 +0545

    Add files via upload

--- CalssicalDist/FermiDirac/main.c
+++ CalssicalDist/FermiDirac/main.c
@@ -1,27 +1,28 @@
 #include<stdio.h>
 #include<math.h>
-FILE *fin, *fout
+FILE *fin, *fout;
 
 double F_D(double, double, double);
 
 int main() {
   int i;
-  unsigned static int n=100;
+  static int n=200;
 // Boltzmann constant 1.3806485279×10-23 J/K
 // Boltzmann constant 8.617330350×10-5 eV/K
-  unsigned static double k_B=8.617330350e-05; // energy in eV
+  static double k_B=8.617330350e-05; // energy in eV
   double E, E_F, T, k_BT;
   fin=fopen("in.dat", "r");
   fout=fopen("out.dat", "w");
-
-
-
+   
+  fscanf(fin,"%lf %lf\n", &E_F, &T);  
+  fclose(fin);
+  printf("%lf %lf\n", E_F, T);
+  k_BT = k_B*T;
   for(i=1; i<=n; i++) {
-    E=0.01*(double) i;
-    fprintf(fout,"%ld %ld\n", E, F_D(E, E_F, k_BT));
+    E=+0.01*(double) i;
+    fprintf(fout,"%lf %lf\n", E, F_D(E, E_F, k_BT));
   }
   fclose(fout);
-  return 0;
 
 }
 
