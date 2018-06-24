c61217329bfb6f91e2aa329cde9df3a6a1b0b72f
Rajendra Adhikari <rpadhikari@gmail.com>
2018-06-24 22:28:04 +0545

    Add files via upload

--- CalssicalDist/BoseE/main.c
+++ CalssicalDist/BoseE/main.c
@@ -2,28 +2,28 @@
 #include<math.h>
 FILE *fin, *fout;
 
-double B_E(double, double, double);
+double fx(double, double, double);
 
 int main() {
   int i;
-  static int n=200;
+  static int n=2000;
 // Boltzmann constant 1.3806485279×10-23 J/K
 // Boltzmann constant 8.617330350×10-5 eV/K
   static double k_B=8.617330350e-05; // energy in eV
-  double E, E_F, T, k_BT;
+  double E, E_F, T1, T2, T3, k_BT;
   fin=fopen("in.dat", "r");
   fout=fopen("out.dat", "w");
-
-  fscanf(fin,"%lf %lf\n", &E_F, &T);
+   
+  fscanf(fin,"%lf %lf %lf %lf\n", &E_F, &T1, &T2, &T3);  
   fclose(fin);
-  printf("%lf %lf\n", E_F, T);
-  k_BT = k_B*T;
+  printf("%lf %lf %lf %lf\n", E_F, T1, T2, T2);
   for(i=1; i<=n; i++) {
-    E=+0.01*(double) i;
-    if(E !=E_F) {
-      fprintf(fout,"%lf %lf\n", E, B_E(E, E_F, k_BT));
+    E=0.001*(double) i;
+    if(fabs(E != E_F)) {
+      fprintf(fout,"%lf %lf %lf %lf\n", E, fx(E,E_F,k_B*T1), 
+            fx(E,E_F,k_B*T2), fx(E,E_F,k_B*T3));
     }
   }
   fclose(fout);
-
 }
+
